5caa208a699b1bf4fd25bf748416eedff4a0ed3a
Wenmo Cui <wenmo_cui@berkeley.edu>
2023-07-14 14:22:54 -0700

    made my first change

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,22 @@
+$(document).ready(function(){
+  getWeather()
+})
+
+function getWeather(searchQuery){
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&appid="+apiKey;
+
+  $(".city").text("");
+  $(".temp").text(data.main.temp);
+
+  $.ajax(url,{success: function(data){
+    console.log(data);
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+  }, error: function(error){
+    $("error-message").text("An error occured")
+  }})
+}
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
@@ -6,4 +25,9 @@ function showPicture(){
 
   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
   
+}
+
+function searchWeather() {
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery)
 }
